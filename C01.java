7582fd47a34f7e41660ab2845d128a8082a2be1a
tjddms134 <tjddms134@nate.com>
2016-10-14 23:08:23 +0900

    Update C01.java

--- C01.java
+++ C01.java
@@ -1,5 +1,5 @@
 package homework3;import java.util.*;
-public class B01 {	
+public class C01 {	
 	int num1, num2, num3;	
 	int max_num, min_num;		
 	public void init() {		
@@ -27,6 +27,6 @@ public class B01 {
 		System.out.println("가장 큰 수는 " + this.max_num + "이고, 가장 작은 수는 " + this.min_num + " 입니다.");
 	}
 	public static void main(String[] args)
-	{		new B01().init();	
+	{		new C01().init();	
 	}	
 }
